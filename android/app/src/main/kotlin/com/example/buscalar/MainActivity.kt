a3e20df99314c80b2e4d0277be16a930158f99f0
luispeixoto <luispeixoto@pq.uenf.br>
2022-04-12 03:29:49 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/buscalar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.buscalar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/login/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:buscalar/app/login/login_store.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  final String title;
+  const LoginPage({Key? key, this.title = 'LoginPage'}) : super(key: key);
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  final LoginStore store = Modular.get();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
